e20906ea42e2839eb9eb94f06967d8d05128a92f
Aquatica <aquathing@protonmail.com>
2021-07-26 18:17:49 +0200

    Cleaner variable names

--- src/CatraProto.Client/MTProto/Auth/SeqnoHandler.cs
+++ src/CatraProto.Client/MTProto/Auth/SeqnoHandler.cs
@@ -18,23 +18,24 @@ namespace CatraProto.Client.MTProto.Auth
 
         public int ComputeSeqno(IObject obj, bool computeServer = false)
         {
-            var side = computeServer ? "server" : "client";
             var add = AcknowledgementHandler.IsContentRelated(obj) ? 1 : 0;
-            var currentSeqno = computeServer ? ContentRelatedReceived : ContentRelatedSent;
-            var newSeqno = currentSeqno + add;
+            var currentCount = computeServer ? ContentRelatedReceived : ContentRelatedSent;
+            var newCount = currentCount + add;
 
             if (computeServer)
             {
-                ContentRelatedReceived = newSeqno;
+                ContentRelatedReceived = newCount;
             }
             else
             {
-                ContentRelatedSent = newSeqno;
+                ContentRelatedSent = newCount;
             }
             
-            var computedNewSeqno = currentSeqno * 2 + add;
-            _logger.Verbose("Computed {Side} seqno for object {Obj}, new value is {NSeqno}, old contentRelated {CRelated}", side, obj, computedNewSeqno, currentSeqno);
-            return computedNewSeqno;
+            var computeSeqno = currentCount * 2 + add;
+            
+            var side = computeServer ? "server" : "client";
+            _logger.Verbose("Computed {Side} seqno for object {Obj}, new value is {NSeqno}, old contentRelated {CRelated}", side, obj, computeSeqno, currentCount);
+            return computeSeqno;
         }
     }
 }
