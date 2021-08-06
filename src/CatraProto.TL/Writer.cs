f4fb6afa3196a3b4170c41008016a1241d3f019f
Aquatica <aquathing@protonmail.com>
2021-08-06 16:30:22 +0200

    Enable nullable in TL Writer

--- src/CatraProto.TL/Writer.cs
+++ src/CatraProto.TL/Writer.cs
@@ -7,133 +7,142 @@ using CatraProto.TL.Interfaces;
 
 namespace CatraProto.TL
 {
-	public class Writer : IDisposable
-	{
-		private ObjectProvider _provider;
+    public class Writer : IDisposable
+    {
+        private readonly ObjectProvider? _provider;
+        private readonly BinaryWriter _writer;
 
-		private BinaryWriter _writer;
+        public Writer(ObjectProvider? provider, Stream stream, bool leaveOpen = false) : this(provider, stream, Encoding.UTF8, leaveOpen)
+        {
+        }
 
-		public Writer(ObjectProvider provider, Stream stream, bool leaveOpen = false) : this(provider, stream, Encoding.UTF8, leaveOpen)
-		{
-		}
+        public Writer(ObjectProvider? provider, Stream stream, Encoding encoding, bool leaveOpen = false)
+        {
+            _provider = provider;
+            _writer = new BinaryWriter(stream, encoding, leaveOpen);
+        }
 
-		public Writer(ObjectProvider provider, Stream stream, Encoding encoding, bool leaveOpen = false)
-		{
-			_provider = provider;
-			_writer = new BinaryWriter(stream, encoding, leaveOpen);
-		}
+        public Stream Stream
+        {
+            get
+            {
+                _writer.Flush();
+                return _writer.BaseStream;
+            }
+        }
 
-		public Stream Stream
-		{
-			get
-			{
-				_writer.Flush();
-				return _writer.BaseStream;
-			}
-		}
+        public void Dispose()
+        {
+            _writer?.Dispose();
+        }
 
-		public void Dispose()
-		{
-			_writer?.Dispose();
-		}
+        public void Write<T>(T value)
+        {
+            if (value == null)
+            {
+                throw new SerializationException($"{nameof(value)} is null", SerializationException.SerializationErrors.ValueNull);
+            }
 
-		public void Write<T>(T value)
-		{
-			if (value == null)
-			{
-				throw new SerializationException($"{nameof(value)} is null", SerializationException.SerializationErrors.ValueNull);
-			}
-			
-			switch (value)
-			{
-				case int i:
-					_writer.Write(i);
-					break;
-				case long l:
-					_writer.Write(l);
-					break;
-				case string s:
-					Write(Encoding.UTF8.GetBytes(s));
-					break;
-				case double d:
-					_writer.Write(d);
-					break;
-				case byte b:
-					_writer.Write(b);
-					break;
-				case byte[] bb:
-					WriteBytes(bb);
-					break;
-				case bool b:
-					WriteBool(b);
-					break;
-				case System.Numerics.BigInteger bigInteger:
-					_writer.Write(bigInteger.ToByteArray());
-					break;
-				case IObject obj:
-					obj.Serialize(this);
-					break;
-				default:
-					throw new SerializationException($"The type {typeof(T)} is not supported",
-						SerializationException.SerializationErrors.TypeNotFound);
-			}
-		}
+            switch (value)
+            {
+                case int i:
+                    _writer.Write(i);
+                    break;
+                case long l:
+                    _writer.Write(l);
+                    break;
+                case string s:
+                    Write(Encoding.UTF8.GetBytes(s));
+                    break;
+                case double d:
+                    _writer.Write(d);
+                    break;
+                case byte b:
+                    _writer.Write(b);
+                    break;
+                case byte[] bb:
+                    WriteBytes(bb);
+                    break;
+                case bool b:
+                    WriteBool(b);
+                    break;
+                case System.Numerics.BigInteger bigInteger:
+                    _writer.Write(bigInteger.ToByteArray());
+                    break;
+                case IObject obj:
+                    obj.Serialize(this);
+                    break;
+                default:
+                    throw new SerializationException($"The type {typeof(T)} is not supported", SerializationException.SerializationErrors.TypeNotFound);
+            }
+        }
 
-		private void WriteBool(bool value)
-		{
-			var type = value ? _provider.BoolTrue : _provider.BoolFalse;
-			if (type == null)
-			{
-				throw new SerializationException("The provider returned a null value for BoolTrue or BoolFalse",
-					SerializationException.SerializationErrors.BoolNull);
-			}
+        private void WriteBool(bool value)
+        {
+            ThrowIfProviderNull();
+            var type = value ? _provider!.BoolTrue : _provider!.BoolFalse;
+            if (type == null)
+            {
+                throw new SerializationException("The provider returned a null value for BoolTrue or BoolFalse", SerializationException.SerializationErrors.BoolNull);
+            }
 
-			((IObject)Activator.CreateInstance(type))?.Serialize(this);
-		}
+            ((IObject)Activator.CreateInstance(type)!).Serialize(this);
+        }
 
-		private void WriteBytes(byte[] bytes)
-		{
-			var arrayLenght = bytes.Length;
-			var totalLenght = arrayLenght;
-			if (arrayLenght <= 253)
-			{
-				_writer.Write((byte)arrayLenght);
-				totalLenght++;
 
-				_writer.Write(bytes);
-			}
-			else
-			{
-				_writer.Write((byte)254);
-				_writer.Write((byte)arrayLenght);
-				_writer.Write((byte)(arrayLenght >> 8));
-				_writer.Write((byte)(arrayLenght >> 16));
-				totalLenght += 4;
+        private void WriteBytes(byte[] bytes)
+        {
+            var arrayLenght = bytes.Length;
+            var totalLenght = arrayLenght;
+            if (arrayLenght <= 253)
+            {
+                _writer.Write((byte)arrayLenght);
+                totalLenght++;
 
-				_writer.Write(bytes);
-			}
+                _writer.Write(bytes);
+            }
+            else
+            {
+                _writer.Write((byte)254);
+                _writer.Write((byte)arrayLenght);
+                _writer.Write((byte)(arrayLenght >> 8));
+                _writer.Write((byte)(arrayLenght >> 16));
+                totalLenght += 4;
 
-			var i = 0;
-			while ((totalLenght + i) % 4 != 0)
-			{
-				i++;
-				_writer.Write((byte)0);
-			}
-		}
-		
-		public void Write<T>(IList<T> list)
-		{
-			Write(_provider.VectorId);
-			WriteNakedVector(list);
-		}
-		
-		public void WriteNakedVector<T>(IList<T> list)
-		{
-			Write(list.Count);
-			foreach (var element in list)
-			{
-				Write(element);
-			}
-		}
-	}
+                _writer.Write(bytes);
+            }
+
+            var i = 0;
+            while ((totalLenght + i) % 4 != 0)
+            {
+                i++;
+                _writer.Write((byte)0);
+            }
+        }
+
+        public void Write<T>(IList<T> list)
+        {
+            ThrowIfProviderNull();
+            Write(_provider!.VectorId);
+
+            WriteNakedVector(list);
+        }
+
+        public void WriteNakedVector<T>(IList<T> list)
+        {
+            Write(list.Count);
+            foreach (var element in list)
+            {
+                Write(element);
+            }
+        }
+
+        private void ThrowIfProviderNull()
+        {
+            if (_provider is null)
+            {
+                throw new NullReferenceException("Provide an object provider in the constructor");
+            }
+        }
+    }
 }
