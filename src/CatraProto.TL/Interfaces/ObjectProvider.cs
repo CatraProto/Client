8d69d9d912186588facfac89db3acf62359a2137
Aquatica <aquathing@protonmail.com>
2021-08-11 16:49:39 +0200

    Nullability improvements

--- src/CatraProto.TL/Interfaces/ObjectProvider.cs
+++ src/CatraProto.TL/Interfaces/ObjectProvider.cs
@@ -1,26 +1,24 @@
-using System;
-
 namespace CatraProto.TL.Interfaces
 {
-	public abstract class ObjectProvider
-	{
-		public abstract Type BoolTrue { get; init; }
-		public abstract Type BoolFalse { get; init; }
-		public abstract int VectorId { get; init; }
+    public abstract class ObjectProvider
+    {
+        public abstract int BoolTrueId { get; init; }
+        public abstract int BoolFalseId { get; init; }
+        public abstract int VectorId { get; init; }
 
-		/// <summary>
-		///     Provides an instance of the class assigned to its constructorId
-		/// </summary>
-		/// <param name="constructorId"></param>
-		/// <returns>
-		///     Returns null if the constructorId is not assigned to any class otherwise, an instance of the class.
-		/// </returns>
-		public abstract IObject? ResolveConstructorId(int constructorId);
+        /// <summary>
+        ///     Provides an instance of the class assigned to its constructorId
+        /// </summary>
+        /// <param name="constructorId"></param>
+        /// <returns>
+        ///     Returns null if the constructorId is not assigned to any class otherwise, an instance of the class.
+        /// </returns>
+        public abstract IObject? ResolveConstructorId(int constructorId);
 
-		protected virtual bool InternalResolveConstructorId(int constructorId, out IObject? obj)
-		{
-			obj = null;
-			return false;
-		}
-	}
+        protected virtual bool InternalResolveConstructorId(int constructorId, out IObject? obj)
+        {
+            obj = null;
+            return false;
+        }
+    }
 }
