47bd10e9d24b61f87b53f93aecb6b5d4b930188c
Aquatica <aquathing@protonmail.com>
2021-08-11 16:40:53 +0200

    Read and save authkey

--- src/CatraProto.Client/MTProto/Auth/AuthKeyHandler/PermanentAuthKey.cs
+++ src/CatraProto.Client/MTProto/Auth/AuthKeyHandler/PermanentAuthKey.cs
@@ -1,18 +1,30 @@
 using System.Threading;
 using System.Threading.Tasks;
-using CatraProto.Client.MTProto.Session.Interfaces;
-using CatraProto.TL;
+using CatraProto.Client.Connections;
+using CatraProto.Client.MTProto.Session.Models;
 using Serilog;
 
 namespace CatraProto.Client.MTProto.Auth.AuthKeyHandler
 {
-    class PermanentAuthKey : ISessionSerializer
+    class PermanentAuthKey
     {
         private readonly AuthKey _authKey;
+        private readonly AuthKeyData _authKeyData;
 
-        public PermanentAuthKey(Api api, ILogger logger)
+        public PermanentAuthKey(ConnectionInfo connectionInfo, SessionData sessionData, Api api, ILogger logger)
         {
-            _authKey = new AuthKey(api, logger);
+            var authKeyData = sessionData.AuthorizationKeys.GetAuthKey(connectionInfo.DcId, out var justCreated);
+            if (!justCreated)
+            {
+                var (key, keyId, salt) = authKeyData.GetData()!.Value;
+                _authKey = new AuthKey(key, keyId, salt, api, logger);
+            }
+            else
+            {
+                _authKey = new AuthKey(api, logger);
+            }
+
+            _authKeyData = authKeyData;
         }
 
         public async Task<AuthKey> GetAuthKeyAsync(CancellationToken cancellationToken = default)
@@ -20,19 +32,10 @@ namespace CatraProto.Client.MTProto.Auth.AuthKeyHandler
             if (_authKey.RawAuthKey is null)
             {
                 await _authKey.ComputeAuthKey(-1, cancellationToken);
+                _authKeyData.SetData(_authKey.RawAuthKey!, _authKey.AuthKeyId!.Value, _authKey.ServerSalt!.Value);
             }
 
             return _authKey;
-        }
-
-        public void Read(Reader reader)
-        {
-            _authKey.Read(reader);
-        }
-
-        public void Save(Writer writer)
-        {
-            _authKey.Save(writer);
         }
     }
 }
