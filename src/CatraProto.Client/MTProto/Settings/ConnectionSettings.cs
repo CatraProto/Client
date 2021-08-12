b78a58b76af1c66bdfdb8ef768e36a775ffc237d
Aquatica <aquathing@protonmail.com>
2021-08-12 20:07:45 +0200

    Use test dcs

--- src/CatraProto.Client/MTProto/Settings/ConnectionSettings.cs
+++ src/CatraProto.Client/MTProto/Settings/ConnectionSettings.cs
@@ -5,7 +5,7 @@ namespace CatraProto.Client.MTProto.Settings
 {
     public class ConnectionSettings
     {
-        public ConnectionInfo DefaultDatacenter { get; } = new ConnectionInfo(IPAddress.Parse( /*"149.154.167.92"*/"149.154.167.50"), 443, 2);
+        public ConnectionInfo DefaultDatacenter { get; } = new ConnectionInfo(IPAddress.Parse( /*"149.154.167.92"*/ /*"149.154.167.50"*/"149.154.167.40"), 443, 2);
         public bool Ipv6Allowed { get; }
 
         public ConnectionSettings(bool ipv6Allowed = false, ConnectionInfo? defaultDatacenter = null)
