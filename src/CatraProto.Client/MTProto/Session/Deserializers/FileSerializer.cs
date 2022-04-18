fb92f6175292a7cbfd1366a615ed7755e3eb2369
Aquatica <aquathing@protonmail.com>
2022-04-18 12:00:01 +0200

    Bundle file serializer

---
+++ src/CatraProto.Client/MTProto/Session/Deserializers/FileSerializer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CatraProto.Client.MTProto.Session.Interfaces;
+using Serilog;
+
+namespace CatraProto.Client.MTProto.Session.Deserializers
+{
+    public class FileSerializer : IAsyncSessionSerializer
+    {
+        private readonly string _filePath;
+        public FileSerializer(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public Task<byte[]> ReadAsync(ILogger logger, CancellationToken token)
+        {
+            logger.Information("Reading session file {FileName}", _filePath);
+            if (!File.Exists(_filePath))
+            {
+                using (var fileStream = File.Create(_filePath))
+                {
+                    return Task.FromResult(Array.Empty<byte>());
+                }
+            }
+            return File.ReadAllBytesAsync(_filePath, token);
+        }
+
+        public Task SaveAsync(byte[] data, ILogger logger, CancellationToken token)
+        {
+            logger.Information("Writing session file {FileName}. Total length {FileLength} bytes", _filePath, data.Length);
+            return File.WriteAllBytesAsync(_filePath, data, token);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
