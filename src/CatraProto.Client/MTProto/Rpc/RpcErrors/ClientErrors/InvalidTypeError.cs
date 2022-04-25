b69a08dbc7989e32f6f8d41d812ab97e05126e07
Aquatica <aquathing@protonmail.com>
2022-04-25 17:19:44 +0200

    new internal error

---
+++ src/CatraProto.Client/MTProto/Rpc/RpcErrors/ClientErrors/InvalidTypeError.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatraProto.Client.MTProto.Rpc.Interfaces;
+
+namespace CatraProto.Client.MTProto.Rpc.RpcErrors.ClientErrors
+{
+    public class InvalidTypeError : RpcError
+    {
+        public override string ErrorDescription { get; } = "Invalid type specified";
+
+        public InvalidTypeError() : base("INVALID_TYPE_ERROR", -10400)
+        {
+        }
+    }
+}
