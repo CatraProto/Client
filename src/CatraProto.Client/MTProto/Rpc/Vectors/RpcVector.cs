ea2904357d11e6f71235b4914fc849391bb36f9f
Aquatica <aquathing@protonmail.com>
2022-04-25 17:20:07 +0200

    Inherit constructors from List<T> in RpcVector<T>

--- src/CatraProto.Client/MTProto/Rpc/Vectors/RpcVector.cs
+++ src/CatraProto.Client/MTProto/Rpc/Vectors/RpcVector.cs
@@ -5,6 +5,20 @@ namespace CatraProto.Client.MTProto.Rpc.Vectors
 {
     public class RpcVector<T> : List<T>, IRpcVector
     {
+        public RpcVector()
+        {
+
+        }
+        public RpcVector(int capacity) : base(capacity)
+        {
+
+        }
+
+        public RpcVector(IEnumerable<T> initial) : base(initial)
+        {
+
+        }
+
         public void Cast(IEnumerable<object> list)
         {
             foreach (T o in list)
