09f6c0fe81bf6877503e3c1251cf742b561bd5fc
Aquatica <aquathing@protonmail.com>
2022-06-11 19:10:52 +0200

    Rename error

--- src/CatraProto.Client/MTProto/Rpc/RpcErrors/ClientErrors/SerializationFailedError.cs
+++ src/CatraProto.Client/MTProto/Rpc/RpcErrors/ClientErrors/SerializationFailedError.cs
@@ -20,11 +20,11 @@ using CatraProto.Client.MTProto.Rpc.Interfaces;
 
 namespace CatraProto.Client.MTProto.Rpc.RpcErrors.ClientErrors
 {
-    internal class SerializationFailed : RpcError
+    public class SerializationFailedError : RpcError
     {
         public override string ErrorDescription { get; }
 
-        public SerializationFailed(string parserError) : base("SERIALIZATION_FAILED", -10400)
+        internal SerializationFailedError(string parserError) : base("SERIALIZATION_FAILED", -10400)
         {
             ErrorDescription = parserError;
         }
