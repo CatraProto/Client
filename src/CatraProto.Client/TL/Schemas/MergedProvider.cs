807a7e188bcecf5a5fb487894ab196f09359a771
Aquatica <aquathing@protonmail.com>
2022-06-13 17:43:54 +0200

    Uncomment

--- src/CatraProto.Client/TL/Schemas/MergedProvider.cs
+++ src/CatraProto.Client/TL/Schemas/MergedProvider.cs
@@ -33,7 +33,7 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-/*
+
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -3155,4 +3155,4 @@ namespace CatraProto.Client.TL.Schemas
             return null;
         }
     }
-}*/
+}
