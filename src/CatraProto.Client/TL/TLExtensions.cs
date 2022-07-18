c56ccf23050f2f65b55908903dd67dd819919910
Aquatica <aquathing@protonmail.com>
2022-07-18 18:29:58 +0200

    Add $type field in Json-Serialized objects

--- src/CatraProto.Client/TL/TLExtensions.cs
+++ src/CatraProto.Client/TL/TLExtensions.cs
@@ -16,16 +16,45 @@ You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using CatraProto.Client.Crypto;
+using CatraProto.Client.TL.Schemas;
+using CatraProto.TL;
 using CatraProto.TL.Interfaces;
+using CatraProto.TL.Results;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace CatraProto.Client.TL
 {
-    public static class TlExtensions
+    public static class TLExtensions
     {
+        private static readonly TLNameSerializer _tlNameSerializer = new TLNameSerializer();
         public static string ToJson(this IObject obj, Formatting formatting = Formatting.Indented)
         {
-            return JsonConvert.SerializeObject(obj, formatting);
+            return JsonConvert.SerializeObject(obj, formatting, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Objects,
+                SerializationBinder = _tlNameSerializer
+            });
+        }
+
+        public static WriteResult ToRecyclableStream(this IObject obj, ObjectProvider provider, out MemoryStream? stream)
+        {
+            stream = MemoryHelper.RecyclableMemoryStreamManager.GetStream();
+            using var writer = new Writer(provider, stream, true);
+            var tryWrite = writer.WriteObject(obj);
+            stream.Seek(0, SeekOrigin.Begin);
+            if (tryWrite.IsError)
+            {
+                stream.Dispose();
+                stream = null;
+            }
+
+            return tryWrite;
         }
     }
 }
