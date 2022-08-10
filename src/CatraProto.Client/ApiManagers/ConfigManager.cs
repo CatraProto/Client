d116a6a2732bd56eec9d9aea168775c4c0f8d8c2
Aquatica <aquathing@protonmail.com>
2022-08-10 19:31:04 +0200

    Clone config object instead of returning directly from cache

--- src/CatraProto.Client/ApiManagers/ConfigManager.cs
+++ src/CatraProto.Client/ApiManagers/ConfigManager.cs
@@ -60,7 +60,7 @@ namespace CatraProto.Client.ApiManagers
                     }
                 }
 
-                return _lastConfig;
+                return (Config)_lastConfig.Clone()!;
             }
         }
 
