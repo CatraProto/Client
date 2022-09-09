6ae6ac80a6e2bc2173e32efa6bce8e6ac18bc9d7
Aquatica <aquathing@protonmail.com>
2022-09-09 17:02:04 +0200

    Add FileProgressCallback and FileProgressInvoker don't notify about progress made while uploading or downloading files

---
+++ src/CatraProto.Client/ApiManagers/Files/FileProgressCallback.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CatraProto.Client.ApiManagers.Files;
+
+public class FileProgressCallback
+{
+    internal Func<long, Task> Callback { get; }
+
+    public FileProgressCallback(Func<long, Task> callback)
+    {
+        Callback = callback;
+    }
+
+    public FileProgressCallback(Action<long> callback)
+    {
+        Callback = (progress) =>
+        {
+            callback(progress);
+            return Task.CompletedTask;
+        };
+    }
+}
---
+++ src/CatraProto.Client/ApiManagers/Files/FileProgressInvoker.cs
@@ -0,0 +1,27 @@
+﻿using CatraProto.Client.Tools;
+using Serilog;
+
+namespace CatraProto.Client.ApiManagers.Files;
+
+internal class FileProgressInvoker
+{
+    private readonly SequentialInvoker _sequentialInvoker;
+    private readonly FileProgressCallback _callback;
+    private readonly object _mutex = new object();
+    private long _currentProgress = 0;
+
+    public FileProgressInvoker(FileProgressCallback callback, ILogger logger)
+    {
+        _callback = callback;
+        _sequentialInvoker = new SequentialInvoker(logger);
+    }
+
+    public void Invoke(long increaseBy)
+    {
+        lock (_mutex)
+        {
+            _currentProgress += increaseBy;
+            _sequentialInvoker.Invoke(() => _callback.Callback(_currentProgress));
+        }
+    }
+}
