2baf5b30b977fa9f7350f4ab51bfcecbe5c2dc28
Aquatica <aquathing@protonmail.com>
2022-09-09 17:15:23 +0200

    Add FileTooBigError

---
+++ src/CatraProto.Client/MTProto/Rpc/RpcErrors/ClientErrors/Files/FileTooBigError.cs
@@ -0,0 +1,13 @@
+﻿using CatraProto.Client.MTProto.Rpc.Interfaces;
+
+namespace CatraProto.Client.MTProto.Rpc.RpcErrors.ClientErrors.Files;
+
+public class FileTooBigError : RpcError
+{
+    public override string ErrorDescription { get; }
+
+    public FileTooBigError(string errorDescription = "The file you've tried to upload exceeds the maximum allowed size") : base("FILE_TOO_BIG", 413)
+    {
+        ErrorDescription = errorDescription;
+    }
+}
