8a7bf7ea91f593a1dd52d82c11179120ce901006
Aquatica <aquathing@protonmail.com>
2022-09-09 16:51:51 +0200

    Add FileManager and UpdateFileId to notify about file_id changes

---
+++ src/CatraProto.Client/ApiManagers/Files/FileManager.cs
@@ -0,0 +1,42 @@
+﻿// CatraProto, a C# library that implements the MTProto protocol and the Telegram API.
+// Copyright (C) 2022 Aquatica <aquathing@protonmail.com>
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+using System;
+using System.Threading.Tasks;
+using CatraProto.Client.ApiManagers.Files.Updates;
+using CatraProto.Client.TL.Schemas.CloudChats;
+using CatraProto.Client.Tools;
+
+namespace CatraProto.Client.ApiManagers.Files;
+
+public class FileManager
+{
+    private readonly TelegramClient _client;
+    private readonly SequentialInvoker _sequentialInvoker;
+
+    public FileManager(TelegramClient client)
+    {
+        _sequentialInvoker = new SequentialInvoker(client.GetLogger<FileManager>());
+        _client = client;
+    }
+
+    internal void NotifyNewFileId(byte[] oldFileId, byte[] newFileId)
+    {
+        _sequentialInvoker.Invoke(() =>
+        {
+            var eventHandler = _client.EventHandler;
+            return eventHandler is null ? Task.CompletedTask : eventHandler.OnUpdateFileId(new UpdateFileId(new FileId(oldFileId), new FileId(newFileId)));
+        });
+    }
+}
---
+++ src/CatraProto.Client/ApiManagers/Files/Updates/UpdateFileId.cs
@@ -0,0 +1,27 @@
+﻿// CatraProto, a C# library that implements the MTProto protocol and the Telegram API.
+// Copyright (C) 2022 Aquatica <aquathing@protonmail.com>
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+namespace CatraProto.Client.ApiManagers.Files.Updates;
+
+public class UpdateFileId
+{
+    public FileId OldFileId { get; }
+    public FileId NewFileId { get; }
+
+    internal UpdateFileId(FileId oldFileId, FileId newFileId)
+    {
+        OldFileId = oldFileId;
+        NewFileId = newFileId;
+    }
+}
